d4803bb937208745d8e67dfb460c0bef8332106d
hisashi-y <82098691+hisashi-y@users.noreply.github.com>
2021-12-26 10:58:14 +0900

    Update app.js

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -2,6 +2,7 @@ function App() {
     const { Container, Row, Col } = ReactBootstrap;
     return (
         <Container>
+            <div class="text">ToDo App by Hisashi Yoshizawa</div>
             <Row>
                 <Col md={{ offset: 3, span: 6 }}>
                     <TodoListCard />
